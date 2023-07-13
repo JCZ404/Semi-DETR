6134100f1b0d3b83dd13991ea70bb60384ce25e6
zhangjiacheng04 <zhangjiacheng04@szbddeMacBook-Pro.local>
2023-07-13 20:55:35 +0800

    update readme.md

--- setup_env.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/root/paddlejob/workspace/env_run/output/temp/envs/anaconda3/envs/mmdet/bin:$PATH
-export TORCH_HOME=/root/paddlejob/workspace/env_run/output/temp/.cache
-export MKL_SERVICE_FORCE_INTEL=1
-export MKL_THREADING_LAYER=GNU
-export PYTHONBREAKPOINT=0
